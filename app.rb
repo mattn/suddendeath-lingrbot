309b9da70f71682fa6d33e9ce8dd740781cd1006
mattn <mattn.jp@gmail.com>
2013-04-14 00:22:43 +0900

    First import

---
+++ app.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+require 'json'
+require 'sinatra'
+require 'sudden_death'
+
+get '/' do
+  "hello"
+end
+
+post '/lingr' do
+  json = JSON.parse(request.body.string)
+  ret = ""
+  json["events"].each do |e|
+    text = e['message']['text']
+    if text =~ /^(突然の.+)$/
+      ret = $1.sudden_death
+    end
+  end
+  ret.rstrip
+end
